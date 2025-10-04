265b58efe3418a8943dfdd2c926dc3e96a0edc3f
Rich Mathias <rich.mathias@cantab.net>
2025-10-04 17:24:19 +0100

    first round of changes to update prerec schedule

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express')
 const app = express()
+app.use(express.json());
 const port = process.env.PORT
 
 const fs = require('node:fs/promises');
@@ -20,6 +21,7 @@ const logic = {
     '103': 'and not',
     '104': 'or not'
 };
+const exec = require('node:child-process');
 
 const connectObs = async () => {
     try {
@@ -38,6 +40,10 @@ const connectObs = async () => {
     }
 }
 
+const enableObsRestartFlag = async(disable) => {
+    return true;
+}
+
 async function getSceneMedia(sceneUuid) {
     const sceneItems = (await obs.call('GetSceneItemList', 
         { sceneUuid: sceneUuid })).sceneItems;
@@ -54,6 +60,35 @@ async function getSceneMedia(sceneUuid) {
         .flat()
         .filter(item => item !== null && item !== undefined);
 }
+
+app.post('/macro/enable', async (req, res) => {
+
+    if(req.get(HEADER_APIKEY) !== process.env.OBS_APIKEY) {
+        res.status(401).send('API key was wrong or missing');
+        return;
+    }
+    isObsConnected || await connectObs();
+
+    const macroName = req.body.name;
+    const macroState = req.body.state == "enabled" ? true : req.body.state == "disabled" ? false : null;
+    if(macroState === null) {
+        res.status(400).send('Coundn\'t understand input');
+        return;
+    }
+    const profileSettings = JSON.parse(await fs.readFile(profilePath, 
+        { encoding: 'utf8' }));
+    const macros = profileSettings.modules['advanced-scene-switcher']
+        .macros.filter(macro => macro.name === macroName);
+    if(macros.length === 0) {
+        res.status(400).send(`Coundn\'t find macro with name ${macroName}`);
+        return;
+    }
+    macros.map(macro => macro.pause = !macroState);
+    await fs.writeFile(profilePath, JSON.stringify(profileSettings, null, 4));
+    enableObsRestartFlag();
+    res.send(macros);
+
+});
 
 app.get('/summary', async (req, res) => {
 
@@ -102,6 +137,67 @@ app.get('/summary', async (req, res) => {
 
 })
 
+function updatePrerecViaFile(djName, date, path) {
+    
+    await fs.copyFile(profilePath, profilePath + `.${dateNow}`);
+
+    // find prerec macro 
+
+    const profileSettings = JSON.parse(await fs.readFile(profilePath, 
+        { encoding: 'utf8' }));
+    const macros = profileSettings.modules['advanced-scene-switcher']
+        .macros.filter(macro => macro.name === macroName);
+    if(macros.length === 0) {
+        console.error(`Coundn\'t find macro with name ${macroName}`);
+        return;
+    }
+    const macro = macros[0];
+
+    macro.condition.dateTime = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][airDate.getDay()] + ' ' + 
+        ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][airDate.getMonth()] + ' ' +
+        airDate.getDate() + ' ' +
+        airDate.getHour() + ':' + airDate.getMinute() + ":" + airDate.getSecond() + ' '
+        airDate.getYear();
+    await fs.writeFile(profilePath, profileSettings, { encoding: 'utf8' });
+
+}
+
+async function updatePrerecViaObs(djName, path) {
+
+    const itemName = process.env.OBS_PREREC_SOURCE_PREFIX + djName;
+    await obs.call('SetInputSettings', { itemName, { file: PLAYLIST_PATH + '/' + path } });
+
+}
+
+const shutdowonObs = async () => await exec('osascript -e \'quit app "OBS"\'');
+const startupObs = async () => await exec('open -a OBS');
+
+app.post('/prerec_refresh', async (req, res) => {
+
+    if(req.get(HEADER_APIKEY) !== process.env.OBS_APIKEY) {
+        res.status(401).send('API key was wrong or missing');
+        return;
+    }
+    isObsConnected || await connectObs();
+
+    const regex = /codesouth (.*) ([0-9\-]+)\.(mp3|m4a)$/;
+    const dateNow = Date.now();
+    const prerecUpdates = fs
+        .readdirDync(
+            process.env.PLAYLIST_PATH, 
+            { recursive: true })
+        .map(file => file.match(regex))
+        .filter(matches => Date.parse(matches[2] > dateNow));
+
+    prerecUpdates.map(update => updatePrerecViaObs(update[1], update[0]));
+    await shutdowonObs();
+    prerecUpdates.map(update => updatePrerecViaFile(update[1], Date.parse(update[2]), update[0]));
+    await startupObs();
+
+    res.status(200).send();
+
+});
+
 app.listen(port, () => {
     console.log(`OBS CS app listening on port ${port}`)
-})
+});
