b15be9bf18facd61112fa781954ea9b8f0360447
Rich Mathias <rich.mathias@cantab.net>
2026-07-01 21:58:18 +0100

    add https code

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
+app.use(cors());
 app.use(express.json());
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const credentials = {
+        key: fs.readFileSync(process.env.PRIVATE_KEY, 'utf8'),
+        cert: fs.readFileSync(process.env.CERT, 'utf8')
+};
 const port = process.env.PORT;
 const { summary, prerecRefresh } = require('./handlers');
 
 app.get('/summary', summary);
-app.get('/prerec_refresh', prerecRefresh);
 
-app.listen(port, () => {
-    console.log(`OBS CS app listening on port ${port}`)
-});
+http.createServer(app).listen(process.env.HTTP_PORT);
+https.createServer(credentials, app).listen(process.env.HTTPS_PORT);
